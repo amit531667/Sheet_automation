6c567792c0ec44a2828cfa88500a1e93c03e0bdd
Amit Kumar Yadav <89924016+amit531667@users.noreply.github.com>
2022-06-20 12:53:02 +0530

    Update all_projects.py

--- all_projects.py
+++ all_projects.py
@@ -19,7 +19,6 @@ def data_frame(url,array_key):
 
     url = url
 
-    #try:
     request = Request(url, headers=headersParam)
 
     a = urlopen(request).read()
@@ -29,34 +28,16 @@ def data_frame(url,array_key):
     return df
 
 
-def uncle_delivery():
-    headersParam = {
-        "username": "amit.kumar@taskmo.com",
-        "authorization": "Authorization: Basic YW1pdC5rdW1hckB0YXNrbW8uY29tOlRNYW1pdEAxOTk0"
-    }
 
-    url = 'https://backdashboard.taskmo.in/excel/uncle_delivery'  # view API  360177297434
 
-    request = Request(url, headers=headersParam)
-        # print(request)
-        # if response.status_code == '503':
-        #   print('error.503')
-        # else:
-    a = urlopen(request).read()
-    b = json.loads(a.decode('utf-8'))
-
-    df = pd.json_normalize(b, 'sow')
-    return df
-def upload_to_sheet(data_frame,spreadsheet_key,wks_name):
+def upload_to_sheet(data_frame,spreadsheet_key,wks_name,path):
     import gspread
     from oauth2client.service_account import ServiceAccountCredentials
     from df2gspread import df2gspread as d2g
     scope = ['https://spreadsheets.google.com/feeds']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/mac/Desktop/py/service.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
     gc = gspread.authorize(credentials)
-            #spreadsheet_key="1x9kzKciGXepm39SA5HlviY3_wK2wGDnQGHBR5bgEbjk"
     spreadsheet_key = spreadsheet_key
     wks_name = wks_name
-    #print("checkk")
     d2g.upload(data_frame, spreadsheet_key, wks_name, credentials=credentials, row_names=False, clean=True)
     return "uploaded"
